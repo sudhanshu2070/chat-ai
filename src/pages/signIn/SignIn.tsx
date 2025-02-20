92eaa78c82f90fbd5820746d88494f676e2a7245
sudhanshu2070 <sssallen2016@gmail.com>
2025-02-21 01:20:38 +0530

    SignIn page 0.3

--- src/pages/signIn/SignIn.tsx
+++ src/pages/signIn/SignIn.tsx
@@ -1,40 +1,43 @@
-import React from 'react';
-import './SignIn.css'; 
+import React from "react";
+import "./SignIn.css";
+import cardImage from   "../../assets/images/img1.png"; // Import the card image from the assets folder
 
-const SignIn: React.FC = () => {
-    return (
-        <div className="login-container">
-          <div className="left-panel">
-            <img src="/images/Skull.png" alt="Skull Illustration" className="skull-image" />
-            <h2>Turn your ideas into reality.</h2>
-            <p>Start for free and get attractive offers from the community</p>
-          </div>
-          <div className="right-panel">
-            <div className="logo">Logo</div>
-            <h2>Login to your Account</h2>
-            <p>See what is going on with your business</p>
-            <button className="google-button">Continue with Google</button>
-            <div className="divider">or Sign in with Email</div>
-            <form>
-              <div className="input-group">
-                <input type="email" placeholder="Email" required />
-              </div>
-              <div className="input-group">
-                <input type="password" placeholder="Password" required />
-              </div>
-              <div className="options">
-                <div className="remember">
-                  <input type="checkbox" id="remember-me" />
-                  <label htmlFor="remember-me">Remember Me</label>
-                </div>
-                <a href="#">Forgot Password?</a>
-              </div>
-              <button type="submit" className="login-button">Login</button>
-            </form>
-            <p>Not Registered Yet? <a href="#">Create an account</a></p>
-          </div>
+
+const SignIn = () => {
+  return (
+    <div className="signin-container">
+      <div className="signin-card">
+        <img
+          src={cardImage} // Path to the card image in the public folder
+          alt="Login Card"
+          className="card-image"
+        />
+        <div className="signin-form">
+          <h2>Good Morning!</h2>
+          <form>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" placeholder="Enter your email" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+              />
+            </div>
+            <button type="submit" className="signin-button">
+              Login
+            </button>
+          </form>
+          <p className="signup-link">
+            Don't have an account? <a href="/signup">Register</a>
+          </p>
         </div>
-      );
-    };
-    
+      </div>
+    </div>
+  );
+};
+
 export default SignIn;
